b5ddfb488b1feec93f05fb7199a9f316c8f8403f
rexshimura <mortpauljpm03@gmail.com>
2025-03-04 18:26:19 +0800

    Updated Composer & Env

--- student-api/config/database.php
+++ student-api/config/database.php
@@ -1,10 +1,26 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../'); 
+$dotenv->load();
+
 class Database {
-    private $host = "localhost:3307"; // the port im currently using : 3307
-    private $user = "root";
-    private $password = "";
-    private $dbname = "student_management";
+    private $host;
+    private $user;
+    private $password;
+    private $dbname;
     public $conn;
+
+    public function __construct() {
+        // Load values from .env file
+        $this->host = $_ENV['DB_HOST'];
+        $this->user = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASSWORD'];
+        $this->dbname = $_ENV['DB_NAME'];
+    }
 
     public function connect() {
         $this->conn = new mysqli($this->host, $this->user, $this->password, $this->dbname);
@@ -12,6 +28,7 @@ class Database {
         if ($this->conn->connect_error) {
             throw new Exception("Connection failed: " . $this->conn->connect_error);
         }
+
         return $this->conn;
     }
 }
